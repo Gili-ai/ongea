9a4b85a27292a3a13599f3ff284ed806b1d8269c
Gili-ai <64913059+Gili-ai@users.noreply.github.com>
2020-11-17 12:28:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!How are you doing?')
+  res.send('Hello World!How are you doing? Today is Tuesday')
 }) 
 
 app.listen(port, () => {
